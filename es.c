6010cfe13da880ba8f53a23b34dbf1c293d7ec6a
Kirill (hasys) Gaevskii <hasysin@gmail.com>
2014-03-28 22:20:06 +0100

    Complited 2.1.b and 2.2

--- es.c
+++ es.c
@@ -1,12 +1,14 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #define X_TAG 0
 #define Y_TAG 1
+#define COUNT_TAG 2
 
 int main(int argc, char *argv[]) {
-    unsigned char XB, YB;
+    unsigned char XB;
+    unsigned char YB;
+    unsigned char buffer;
     int numprocs,
         myid,
         i,j;
@@ -17,19 +19,20 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs); /* zjistíme, kolik procesů běží */
     MPI_Comm_rank(MPI_COMM_WORLD, &myid); /* zjistíme id svého procesu */
 
+    numprocs -= 1;
     if (myid == 0) {
-        int numbersTmp[numprocs - 1],
-            numbers[numprocs - 1];
-            
+        char numbersTmp[numprocs],
+            numbers[numprocs];
+
         FILE *fp = fopen("numbers", "r");
-        fread(numbersTmp, 1, numprocs - 1, fp);
+        fread(numbersTmp, 1, numprocs, fp);
         fclose(fp);
         
-        for (i = 0; i < numprocs - 1; i += 1) {
+        for (i = 0; i < numprocs; i += 1) {
             numbers[i] = -1;
         }
 
-        for (i = 0; i < numprocs - 1; i += 1) {
+        for (i = 0; i < numprocs; i += 1) {
             if (!isExistsInArray(numbersTmp[i], numbers)) {
                 numbers[i] = numbersTmp[i];
             } else {
@@ -40,24 +43,35 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        printf("%d: We have %d processors\n", myid, numprocs);
-        for (i = 0; i < numprocs - 1; i += 1) {
-            MPI_Send(&numbers[i], 1, MPI_INT, i+1, X_TAG, MPI_COMM_WORLD);
+        printf("%d: We have %d processors\n", myid, (numprocs + 1));
+        for (i = 0; i < numprocs; i += 1) {
+            MPI_Send(&numprocs, 1, MPI_INT, i + 1, COUNT_TAG, MPI_COMM_WORLD);
         }
 
-        MPI_Send(&numbers[0], 1, MPI_UNSIGNED_CHAR, 1, Y_TAG, MPI_COMM_WORLD);
+        for (i = 0; i < numprocs; i += 1) {
+            MPI_Send(&numbers[i], 1, MPI_INT, i + 1, X_TAG, MPI_COMM_WORLD);
+            MPI_Send(&numbers[i], 1, MPI_INT, 1, Y_TAG, MPI_COMM_WORLD);
+        }
+
     } else {
-        int C = 1, X = -1, Y = -1, Z = -1;
+        int C = 1, X = -1, Y = -1, Z = -1, count = -1;
+
+        MPI_Recv(&buffer, 1, MPI_INT, 0, COUNT_TAG, MPI_COMM_WORLD, &stat);
+        count = buffer;
 
         MPI_Recv(&XB, 1, MPI_INT, 0, X_TAG, MPI_COMM_WORLD, &stat);
         X = XB;
-        printf("X_TAG! Processor %d. Registers values: C=%d, X=%d, Y=%d, Z=%d.\n", myid, C, X, Y, Z);
 
-        if (myid == 1) {
-            MPI_Recv(&YB, 1, MPI_INT, 0, Y_TAG, MPI_COMM_WORLD, &stat);
+        for (i = 0; i < count - myid + 1; i += 1) {
+            MPI_Recv(&YB, 1, MPI_INT, myid - 1, Y_TAG, MPI_COMM_WORLD, &stat);
             Y = YB;
-            printf("Y_TAG! Processor %d. Registers values: C=%d, X=%d, Y=%d, Z=%d.\n", myid, C, X, Y, Z);
+            C = (X > Y) ? (C + 1) : C;
+            if(myid < count) {
+                MPI_Send(&Y, 1, MPI_INT, myid + 1, Y_TAG, MPI_COMM_WORLD);
+            }
         }
+
+        printf("Processor %d. Registers values: C=%d, X=%d, Y=%d, Z=%d.\n", myid, C, X, Y, Z);
     }
 
     MPI_Finalize();
