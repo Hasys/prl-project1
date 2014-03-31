7fbdece45488b0eb299c3953e86b43669594a03d
Kirill (hasys) Gaevskii <hasysin@gmail.com>
2014-03-31 23:38:21 +0200

    Little optimizations.

--- es.c
+++ es.c
@@ -1,21 +1,18 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <sys/time.h>
 
 #define X_TAG 0
 #define Y_TAG 1
 #define Z_TAG 2
-#define COUNT_TAG 3
-#define OUT_TAG 4
+#define OUT_TAG 3
 
 int isExistsInArray (unsigned char, unsigned char *, int);
 
 int main (int argc, char *argv[]) {
-    unsigned char XB, YB, ZB, OB,
-                  buffer;
+    unsigned char XB, YB, ZB, OB;
     int numprocs, computers,
         myid,
-        i, j;
+        i;
 
     MPI_Status stat;
     
@@ -60,7 +57,6 @@ int main (int argc, char *argv[]) {
 
         // (krok 2.1 odesilání zprav)
         for (i = 0; i < computers; i += 1) {
-            MPI_Send(&computers, 1, MPI_INT, i + 1, COUNT_TAG, MPI_COMM_WORLD);
             MPI_Send(&numbers[i], 1, MPI_INT, i + 1, X_TAG, MPI_COMM_WORLD);
             MPI_Send(&numbers[i], 1, MPI_INT, 1, Y_TAG, MPI_COMM_WORLD);
         }
@@ -77,37 +73,35 @@ int main (int argc, char *argv[]) {
     } else {
         // (krok 1)
         int C = 1, X = -1, Y = -1, Z = -1, 
-            count = -1, receiver;
-
-        // Abys vědel kolik má sousedů a je poslední nebo ne.
-        MPI_Recv(&buffer, 1, MPI_INT, 0, COUNT_TAG, MPI_COMM_WORLD, &stat);
-        count = buffer;
+            receiver;
 
         // (krok 2.1 posuv hodnot do registru a posuv všech registrů Y doprava)
         MPI_Recv(&XB, 1, MPI_INT, 0, X_TAG, MPI_COMM_WORLD, &stat);
         X = XB;
         // První má n hodnot od nultého procesoru
-        for (i = 0; i < count; i += 1) {
+        for (i = 0; i < computers; i += 1) {
             MPI_Recv(&YB, 1, MPI_INT, myid - 1, Y_TAG, MPI_COMM_WORLD, &stat);
             Y = YB;
             // (krok 2.2)
             C = (X > Y) ? (C + 1) : C;
-            if (myid < count) {
+            if (myid < computers) {
                 MPI_Send(&Y, 1, MPI_INT, myid + 1, Y_TAG, MPI_COMM_WORLD);
             }
         }
 
-        // Přesuneme hodnoty z registru X do registru s indexem C (krok 2.3)
+        // Přesuneme hodnoty z registru X do registru procesora s indexem C (krok 2.3)
         MPI_Send(&X, 1, MPI_INT, C, Z_TAG, MPI_COMM_WORLD);
         MPI_Recv(&ZB, 1, MPI_INT, MPI_ANY_SOURCE, Z_TAG, MPI_COMM_WORLD, &stat);
         Z = ZB;
 
-        receiver = (myid == count) ? 0 : myid + 1;
+        receiver = (myid == computers) ? 0 : myid + 1;
         
-        // První nic ne čeká ale okamžitě zasilá svou hodnotu z registru Z dál doprava (krok 3)
+        // (krok 3)
         for (i = 1; i < myid; i += 1) {
-            MPI_Recv(&ZB, 1, MPI_INT, myid - 1, OUT_TAG, MPI_COMM_WORLD, &stat);
+            // Nevím jak to možně, ale na marlinovi (jen na merlinovi) ne funguje bez tenhle kroku
+            receiver = (myid == computers) ? 0 : myid + 1;
             MPI_Send(&Z, 1, MPI_INT, receiver, OUT_TAG, MPI_COMM_WORLD);
+            MPI_Recv(&ZB, 1, MPI_INT, myid - 1, OUT_TAG, MPI_COMM_WORLD, &stat);
             Z = ZB;
         }
 
